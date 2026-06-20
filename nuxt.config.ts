4443833266f899d7ae3811800b2abec338aafc8a
zhenhuang-tw <193303856+zhenhuang-tw@users.noreply.github.com>
2026-06-20 14:33:08 +0800

    chore(prettier): configure Prettier and format codebase
    - Add .prettierrc configuration file.
    - Apply formatting rules globally across all files.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,48 +1,48 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-const ASSET_BASE_URL = "https://assets.zhenhuang.tw/tlv";
+const ASSET_BASE_URL = 'https://assets.zhenhuang.tw/tlv'
 
 export default defineNuxtConfig({
-  modules: ["@nuxt/content"],
+  modules: ['@nuxt/content'],
   devtools: { enabled: true },
   future: {
     // 決定專案要採用哪一個主版本的預設行為
     compatibilityVersion: 4,
   },
   // 鎖定 Nuxt 的功能時間點；未來如有破壞性變更，更新於此時點之後，將暫不套用該更新
-  compatibilityDate: "2026-06-11",
+  compatibilityDate: '2026-06-11',
 
-  css: ["@picocss/pico/css/pico.classless.min.css", "~/assets/css/main.css"],
+  css: ['@picocss/pico/css/pico.classless.min.css', '~/assets/css/main.css'],
 
   app: {
     head: {
-      htmlAttrs: { lang: "zh-Hant-TW" },
+      htmlAttrs: { lang: 'zh-Hant-TW' },
       meta: [
-        { charset: "utf-8" },
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
-        { name: "color-scheme", content: "light dark" },
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'color-scheme', content: 'light dark' },
       ],
-      title: "雲端法條本",
+      title: '雲端法條本',
       link: [
         {
-          rel: "apple-touch-icon",
-          sizes: "180x180",
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
           href: `${ASSET_BASE_URL}/apple-touch-icon.png`,
         },
         {
-          rel: "icon",
-          type: "image/png",
-          sizes: "32x32",
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
           href: `${ASSET_BASE_URL}/favicon-32x32.png`,
         },
         {
-          rel: "icon",
-          type: "image/png",
-          sizes: "16x16",
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
           href: `${ASSET_BASE_URL}/favicon-16x16.png`,
         },
         {
-          rel: "manifest",
+          rel: 'manifest',
           href: `${ASSET_BASE_URL}/site.webmanifest`,
         },
       ],
@@ -56,4 +56,4 @@ export default defineNuxtConfig({
       crawlLinks: true,
     },
   },
-});
+})
